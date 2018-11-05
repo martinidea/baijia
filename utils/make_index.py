e34a8032f2cd3536d6f45b5781e17d364e6b299e
martin <martinidea@163.com>
2018-11-05 22:18:19 +0800

    修复

--- utils/make_index.py
+++ utils/make_index.py
@@ -18,7 +18,7 @@ def make_index(page):
         index_html.writelines(
             """<!DOCTYPE html><html lang="zh"><head><meta charset="UTF-8"><title>{0}</title></head>\n<body>\n""".format(
                 os.path.split(page)[-1]))
-        content = ['<a href=\"' + a + '\"  target=\"_blank\">' + a + '</a></br>\n' for a in get_html_in_page(page)]
+        content = ['<a href=\"' + a + '\">' + a + '</a></br>\n' for a in get_html_in_page(page)]
         index_html.writelines(content)
         index_html.writelines("</body></html>")
         index_html.close()
@@ -37,7 +37,7 @@ def make_page_html():
     with open('../index.html', 'w', encoding="utf-8") as page_html:
         page_html.writelines(
             '<!DOCTYPE html><html lang="zh"><head><meta charset="UTF-8"><title>百家号文章</title></head><body><h1>百家号</h1>\n')
-        content = ['<a href=\"html/' + a + '/index.html\"  target=\"_blank\">' + a + '</a></br>\n' for a in get_pages()]
+        content = ['<a href=\"html/' + a + '/index.html\">' + a + '</a></br>\n' for a in get_pages()]
         page_html.writelines(content)
         page_html.writelines("</body></html>")
         get_pages()
